557a656da82d1c77289a004865c8fc3d916d7e87
joshuamullings <joshuamullings@gmail.com>
2024-02-21 16:51:24 +0000

    Update ngrok

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import ngrok from "ngrok";
+//import ngrok from "ngrok";
+import ngrok from "@ngrok/ngrok";
 import bodyParser from "body-parser";
 import crypto from "crypto";
 
@@ -14,16 +15,22 @@ const { NGROK_AUTH_TOKEN, PORT, ZOOM_SECRET_TOKEN } = process.env;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 app.post("/hook", (req, res) => {
     console.log(JSON.stringify(req.headers, null, 4));
     console.log(JSON.stringify(req.body, null, 4));
 
     if (req.body.event === "endpoint.url_validation") {
+        // Zoom validation hash
         const hashForValidate = crypto
             .createHmac("sha256", ZOOM_SECRET_TOKEN)
             .update(req.body.payload.plainToken)
             .digest("hex");
 
+        // Pipedream response format
         res.header("Content-Type", "text/html; charset=utf-8");
         res.header("Connection", "keep-alive");
         res.header("X-Powered-By", "Express");
@@ -46,8 +53,7 @@ app.post("/hook", (req, res) => {
 });
 
 app.listen(PORT, async () => {
-    await ngrok.authtoken(NGROK_AUTH_TOKEN);
-    const url = await ngrok.connect(PORT);
+    const listener = await ngrok.forward({ addr: PORT, authtoken_from_env: true });
     console.log(`ngrok-oauth app running locally on http://localhost:${PORT}`);
-    console.log(`ngrok tunnel running on ${url}`);
+    console.log(`ngrok tunnel running on ${listener.url()}`);
 });
